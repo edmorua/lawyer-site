8d586c66145fb0e8b756be8198c1f3f8fc65c341
ianArlen <ian.street.fut@gmail.com>
2020-12-05 17:35:47 -0600

    Navbar and Hero ready!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Hero from './Components/Hero/Hero'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component ={Hero} />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
 export default App;
+
